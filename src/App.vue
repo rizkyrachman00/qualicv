746587ee24ba3d055076504b5687ff4fd1578cc2
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-08-03 19:34:04 +0700

    feat: init project, setup tailwind v4

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+        <div>
+            <h1 class="text-3xl text-blue-400 text-center pt-10">Hallo App</h1>
+        </div>
+    </main>
+</template>
+
+<style scoped></style>
